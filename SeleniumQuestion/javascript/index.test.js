c5a5289e4894ee61c3cd8f0637690dd0b25094f8
Tony Lawson <tony.lawson@bjss.com>
2020-02-21 15:38:13 +0000

    javascript

---
+++ SeleniumQuestion/javascript/index.test.js
@@ -0,0 +1,21 @@
+const sw = require("selenium-webdriver")
+const chrome = require('selenium-webdriver/chrome')
+const chromedriver = require('chromedriver')
+const path = require('path');
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+const options = new chrome.Options();
+const file = path.join(__dirname, '..', 'index.html');
+const webdriver = new sw.Builder().forBrowser("chrome").setChromeOptions(options).build();
+
+beforeEach(() => {
+  return webdriver.get(file);
+});
+
+afterEach(() => {
+  webdriver.quit();
+});
+
+test('page test', async () => {    
+  expect(await webdriver.getTitle()).toBe("Test me");
+});
---
+++ SeleniumQuestion/javascript/readme.md
@@ -0,0 +1,15 @@
+# Prerequisites 
+
+Chrome
+<br>Node
+<br>NPM
+
+## Install
+Install the packages in the [package.json](package.json) using npm
+<br>Verify the install by running the default test provided, you should see a Chrome browser open and close, as it checks the page title.
+
+## Write tests
+Choose your preferred IDE or Text Editor from the ones available.
+<br>Create each test as a new test method within the provided test file [index.test.js](index.test.js), using the jest standard.
+<br>The beforeeach fixture provides an instantiated `webdriver` object on the target page.
+<br>Run tests on the commandline from the javascript directory
